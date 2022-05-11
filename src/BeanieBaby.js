26676c279b9961b99a5617ed7f86cce6e01d770d
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-10 17:17:22 -0700

    trying to figure this all out

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -2,8 +2,12 @@ import { Link } from 'react-router-dom';
 
 export default function BeanieBaby({ beanieBaby }) {
   return (
-    // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
-    // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    <>
+      <Link to={`/beanie-babies/${beanieBaby.id}`}>Detail Page</Link>
+      <h1>{beanieBaby.title}</h1>
+      <img src={beanieBaby.image}></img>
+    </>
   );
 }
+// it should also render the beanie baby's image and show the beanie baby's name
+// this should contain a react-router-dom Link to the detail page for this particular beanie
--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,15 +1,22 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getSingleBeanie } from './services/fetch-utils';
 
 export default function BeanieDetail() {
   // you'll need to track the current beanieBaby in state
   // you'll need to get the route param using the appropriate react router hook
+  const [beanieBaby, setBeanieBaby] = useState({});
+  const params = useParams();
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
+    async function fetchBeanie() {
+      const data = await getSingleBeanie(params.id);
 
-  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
+      setBeanieBaby(data);
+    }
+    fetchBeanie();
+  }, [params.id]); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes
 
   function handleBeanieClick() {
     // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
@@ -18,15 +25,15 @@ export default function BeanieDetail() {
   return (
     <>
       {/* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */}
-      
-      {/* <Link to='/'>Home</Link>
-      <div className='beanie-detail' onClick={handleBeanieClick}>
-        <div className='beanie-data'>
+
+      <Link to="/">Home</Link>
+      <div className="beanie-detail" onClick={handleBeanieClick}>
+        <div className="beanie-data">
           <p>{beanieBaby.animal}</p>
           <p>{beanieBaby.title}</p>
           <p>Zodiac: {beanieBaby.astroSign}</p>
           <p> Born on {beanieBaby.birthday}</p>
-          <img className='beanie-img' src={beanieBaby.image}/>
+          <img className="beanie-img" src={beanieBaby.image} />
           <p>Color: {beanieBaby.color}</p>
           <p>Release Date: {beanieBaby.releaseDate}</p>
           <p>Retirement Date: {beanieBaby.retirementDate}</p>
@@ -38,7 +45,7 @@ export default function BeanieDetail() {
           <p>Swing Tag Generation: {beanieBaby.swingTagGeneration}</p>
           <p>Tush Tag Generation: {beanieBaby.tushTagGeneration}</p>
         </div>
-      </div>   */}
+      </div>
     </>
   );
 }
