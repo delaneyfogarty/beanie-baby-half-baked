f23ddb33d07c297f4678c9ba6872126fc34f51be
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-12 16:41:50 -0700

    finished app

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -8,7 +8,7 @@ export default function BeanieDetail() {
   // you'll need to get the route param using the appropriate react router hook
   const [beanieBaby, setBeanieBaby] = useState({});
   const params = useParams();
-  const history = useHistory();
+  // const history = useHistory();
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
@@ -20,17 +20,18 @@ export default function BeanieDetail() {
     fetchBeanie();
   }, [params.id]); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes
 
-  function handleBeanieClick() {
-    // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
-    history.push(`/beanies/${params.id}`);
-  }
+  // function handleBeanieClick(e) {
+  //   e.preventDefault();
+  //   // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
+  //   history.push(`https://beaniepedia.com/beanies/beanie-boos/${params.id}/`);
+  // }
 
   return (
     <>
       {/* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */}
 
       <Link to="/">Home</Link>
-      <div className="beanie-detail" onClick={handleBeanieClick}>
+      <div className="beanie-detail">
         <div className="beanie-data">
           <p>{beanieBaby.animal}</p>
           <p>{beanieBaby.title}</p>
